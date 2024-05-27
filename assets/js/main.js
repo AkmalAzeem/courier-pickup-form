00c17f800455bad3f2a6eff0d6f47d998fdcd73e
E024664 <E024664@esoft.academy>
2024-05-27 22:07:42 +0530

    feat: display all localstorage UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,11 @@ import JustValidate from "just-validate";
 
 const formEl = document.getElementById("courier-request-form");
 
+const localStorageKey = "courierData";
+
 const validateForm = new JustValidate(formEl, {
-    validateBeforeSubmitting: true,
-})
+  validateBeforeSubmitting: true,
+});
 
 validateForm.addField(
   "#name",
@@ -21,14 +23,14 @@ validateForm.addField(
       value: 20,
     },
   ],
-  { 
-    errorLabelCssClass: ["form-error"], 
-}
+  {
+    errorLabelCssClass: ["form-error"],
+  }
 );
 
 validateForm.addField(
-  "#mobile-no",
-  [    
+  "#mobile",
+  [
     {
       rule: "required",
     },
@@ -51,11 +53,10 @@ validateForm.addField(
 
 validateForm.addField(
   "#pickup-date",
-  [    
+  [
     {
       rule: "required",
     },
-    
   ],
   {
     errorLabelCssClass: ["form-error"],
@@ -64,45 +65,73 @@ validateForm.addField(
 
 validateForm.addField(
   "#pickup-area",
-  [    
+  [
     {
       rule: "required",
     },
-    
   ],
   {
     errorLabelCssClass: ["form-error"],
   }
 );
 
-
 validateForm.onSuccess((e) => {
+  const formData = new FormData(formEl);
 
-  const formData = new FormData(formEl)
+  const formValueObj = Object.fromEntries(formData.entries());
 
-  const formValueObj = Object.fromEntries(formData.entries())
+  const newCourierData = [];
 
-  const newCourierData = []
+  const existingCourierData = localStorage.getItem("courierData");
 
-  const existingCourierData = localStorage.getItem("courierData")
+  const existingCourierArray = JSON.parse(existingCourierData);
 
-  const existingCourierArray = JSON.parse(existingCourierData)
+  if (existingCourierData) {
+    existingCourierArray.push(formValueObj);
 
-  if (existingCourierData) { 
-    existingCourierArray.push(formValueObj)
+    localStorage.setItem(localStorageKey, JSON.stringify(existingCourierArray));
+  } else {
+    newCourierData.push(formValueObj);
 
-    localStorage.setItem("courierData", JSON.stringify(existingCourierArray))
-  
-  } 
-  else {
-    newCourierData.push(formValueObj)
-
-    localStorage.setItem("courierData", JSON.stringify(newCourierData))
-
+    localStorage.setItem(localStorageKey, JSON.stringify(newCourierData));
   }
 
-  alert("Courier Request submitted successfully")
-  formEl.reset() 
+  alert("Courier Request submitted successfully");
+  formEl.reset();
+});
 
+function getAllCourierDatas() {
+  const courierData = localStorage.getItem(localStorageKey);
 
-})
+  const courierDataArr = JSON.parse(courierData);
+
+  if (courierDataArr) {
+
+  const tableEl = document.querySelector("#courierDataTable");
+
+   const finalData = courierDataArr.map((courierData) => {
+    return `
+        <tr>
+            <td class="px-2 py-1 border">${courierData.name}</td>
+            <td class="px-2 py-1 border">${courierData.mobile}</td>
+            <td class="px-2 py-1 border">${courierData['pickup-date']}</td>
+            <td class="px-2 py-1 border">${courierData['pickup-area']}</td>
+            <td class="px-2 py-1 border">
+                <button
+                    type="button"
+                    class="px-2 py-1 rounded bg-red-500 hover:bg-red-600 text-white text-sm"
+                >
+                    Delete
+                </button>
+                </td>
+        </tr>
+        `;
+
+  }).join(" ");
+
+  tableEl.innerHTML += finalData
+
+  }
+  console.log("No value available on LocalStorage")
+}
+getAllCourierDatas();
